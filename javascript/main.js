ad9a3ea4cfd0a65cb579a1bdefef3ecb35a23cf7
Andrea Bresso <andreabresso@gmail.com>
2022-04-19 16:44:11 -0300

    Created arrays with words to display to the user

--- javascript/main.js
+++ javascript/main.js
@@ -28,8 +28,22 @@ if (age > 0 && age < 18){
     console.log("¡Excelente! Podemos comenzar a jugar");
 }
 
-/* Muestro un texto al usuario para que lo escriba */
-let displayedText = console.log("Este es un texto de prueba");
+/* Creo un Array multidimensional que contiene cuatro arrays con 50 palabras cada uno */
+const wordsArray = [
+    ["constitución", "jurisprudencia", "prestamista", "posición", "apelación", "tribunal", "juez", "ilegalidad", "material", "consecuencias", "recurso", "procesal", "prejudicialidad", "intimación", "público", "notorio", "manifiestamente", "improcedente", "incpacidad", "perentorio", "plazo", "sucesión", "parentesco", "agenda", "económico", "financiero", "inmobiliario", "practicidad", "recurrente", "contraria", "parámetro", "traslado", "instancia", "caducidad", "particular", "querellante", "penalidad", "preventivo", "remedio", "cautelar", "aseguramiento", "intencionalidad", "tasación", "reposición", "sellado", "casación", "contencioso", "administrativo", "homicidio", "agravante"],
+    ["perro", "gato", "delfín", "tortuga", "jirafa", "araña", "oveja", "león", "tigre", "leopardo", "pantera", "mariposa", "cisne", "tero", "tucán", "halcón", "camello", "lobo", "ratón", "ballena", "pez", "escarabajo", "conejo", "vaca", "caballo", "burro", "medusa", "quis", "liebre", "búho", "carancho", "garza", "cotorra", "gorrión", "paloma", "flamenco", "águila", "topo", "iguana", "víbora", "alacrán", "cucaracha", "oso", "zebra", "rinoceronte", "hipopótamo", "sapo", "pato", "gallina", "canario"],
+    ["cuchara", "tenerdor", "cuchillo", "fuente", "olla", "horno", "cocina", "espátula", "batidor", "espumadera", "taza", "cucharón", "bandeja", "tartera", "bowl", "sacacorcho", "abrelatas", "tiernizador", "parrilla", "pincel", "sartén", "tostadora", "microondas", "termo", "cafetara", "pava", "abrelatas", "botella", "tapa", "termómetro", "molde", "pirotin", "colador", "escurridor", "vaso", "copa", "rejilla", "esponja", "repasador", "jarra", "mate", "bombilla", "encendedor", "agarradera", "pinza", "azucarera", "hornalla", "decantador", "calentador", "canasta"],
+    ["pan", "lechuga", "queso", "pepino", "tomate", "rúcula", "chocolate", "uva", "manzana", "banana", "ciruela", "café", "acelga", "pizza", "cebolla", "pimiento", "tarta", "sopa", "pasta", "fideos", "puré", "zapallo", "batata", "papa", "huevo", "pollo", "pescado", "sardina", "atún", "magdalena", "galleta", "torta", "merengue", "manteca", "sal", "pimienta", "panqueque", "bondiola", "carré", "milanesa", "jamón", "tortilla", "bizcochuelo", "pera", "naranja", "mandarina", "aceite", "asado", "costilla", "salsa"]
+]
+
+/* Creo una función que elige uno de esos arrays al azar y se lo muestra al usuario utilizando join */
+function loadWords (wordsArray) {
+    const ranIndex = Math.floor(Math.random() * wordsArray.length);
+    let wordsShowed = wordsArray[ranIndex];
+    console.log(wordsShowed.join(" "));
+}
+
+loadWords(wordsArray);
 
 /* Le indico al usuario dónde está el texto que debe tipear para que lo ingrese */
 let inputText = prompt("Escriba el texto que aparece en la consola");
